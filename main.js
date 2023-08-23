84e4f887cfbdb01c32166ea9ba0ebb64997a0e83
Noxik <64279796+Noxik@users.noreply.github.com>
2023-08-23 22:50:48 +0200

    added some functionalities: 1) show todos in main section after adding; show project names as buttons on aside with ready event listener

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 let todoList = []
-// let projects = []
-
-// nie potrzebuje pomocniczego project bo nazwy projektow moge wyciagnac z obiektu poprzez
-for (let key in todoList) {console.log(todoList[key].project)}
-
-
 
 class Task {
     constructor(project, title, description, dueDate, priority) {
@@ -15,7 +9,6 @@ class Task {
         this.priority = priority}
 }
 
-
 function addNewTask() {
 let a = document.getElementById("project").value  
 let b = document.getElementById("title").value
@@ -25,7 +18,28 @@ let e = document.getElementById("prio").value
     todoList.push(new Task(a,b,c,d,e))
 }
 
-document.getElementById("submit").addEventListener("click", addNewTask)
+
+// need to apply todoList here to work correctly
+function addToMain(taskList) {
+    let div = document.createElement("div");
+
+    for (let key in  taskList[taskList.length-1]) {    
+    // without project name
+    if (key !== "project") {   
+        let span = document.createElement("span");
+    //    console.log(key, [key], [key].toString())
+    span.textContent = taskList[taskList.length-1][key]
+    div.appendChild(span)
+    }
+    }
+   return document.querySelector("main").appendChild(div)
+}
+
+document.getElementById("submit").addEventListener("click", () => {
+    addNewTask();
+    abcd();
+    addToMain(todoList);
+    addEventsToAsideButtons()})
 
 document.querySelector("button").addEventListener("click", () => {
     document.getElementById("newProjectModal").style.display = "block"
@@ -38,54 +52,66 @@ document.getElementById("newNameSubmit").addEventListener("click", () => {
    // new option will be selected as default
     option.selected = true;
     document.getElementById("project").appendChild(option)
-
-    // add new project to projects list
-//    projects.push(document.getElementById("newProjectName").value)
-
+    //cleaning inputs
     document.getElementById("newProjectName").value = ""
     document.getElementById("newProjectModal").style.display = "none"
    
 })
 
-// create filtered arr if project name is equal to sth
-const arx = todoList.filter(function(key) {if (key.project === projects[1]) {return true}})
-
-
 function newTextTag(tag, text) {
     let element = document.createElement(tag);
     element.textContent = text;
-    return document.querySelector("aside").appendChild(element)
+
+    element.classList.add("projectBtn")  // addClass
+    element.setAttribute("id", `asiBtn_${text}`);        // addId
+
+    return document.querySelector("#projectsListing").appendChild(element)
         }
 
 // add to aside buttons from object project names :)
 function abcd() {
-    let test = []
+            // clean project box before add new projects
+            document.querySelector("#projectsListing").textContent = ""
+    let projectNames = []
     for (let key in todoList) {
-    // check if already project exist in aside
-    if (!test.some(e => e === todoList[key].project)) {
-    test.push(todoList[key].project)}}
+    // check if already project exist in projectNames arr
+    if (!projectNames.some(e => e === todoList[key].project)) {
+    // add project name to array only if project name doesnt exist there
+    projectNames.push(todoList[key].project)}}
     
     // if not button will appear
-    test.forEach((xxx) => {    
-    newTextTag("button", xxx)
+    projectNames.forEach((name) => {    
+    newTextTag("button", name);
     })
  }
 
+ //adding Event listener to aside buttons and it return button text content which is equal to project name => use in filtered to show only choose project :)
+function addEventsToAsideButtons() {
+    let proBtn = document.querySelectorAll(".projectBtn")
+    for (let i=0; i<proBtn.length; i++) {
+        proBtn[i].addEventListener("click", function(e) {console.log(proBtn[i].textContent)})}
+}
+
+// create filtered arr if project name is equal to sth
+const arx = todoList.filter(function(key) {if (key.project === projects[1]) {return true}})
+
 
 // NEXT ADD DISPLAY PROJECT TASK AFTER BUTTON KLIK with filter!:)
- 
-/* show projects from []
-function show() {
-projects.forEach((project) => newTextTag("button", project));
-} */
+// CONSIDER TO ADD PROJECT BUTTON AFTER ADDING NEW PROJECT TO NAVBAR
 
-/* same as above but without projects arr
-function showTest() {
-    let test = []
-for (let key in todoList) {
-    test.push(todoList[key].project)}
-    test.forEach((xxx) => {    
-    newTextTag("button", xxx)}
-                );
+/* FIRST function, but i added argument to it because then i can use it to show to do for filtered projects
+function addToMain() {
+    let div = document.createElement("div");
+
+    for (let key in  todoList[todoList.length-1]) {    
+    // without project name
+    if (key !== "project") {   
+        let span = document.createElement("span");
+        console.log(key, [key], [key].toString())
+    span.textContent = todoList[todoList.length-1][key]
+    div.appendChild(span)
+    }
+    }
+   return document.querySelector("main").appendChild(div)
 }
 */
